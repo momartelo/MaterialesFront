946d6beb62cd49baaceb7e34b5547c5686553f0c
Martin <momartelo@yahoo.com.ar>
2024-08-20 19:54:25 -0300

    cambio

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,2 +1,4 @@
-export const API_URL = "http://localhost:3020/api"; // aca va la direccion del backend donde esta la api
-export const IMAGES_API_URL = "http://localhost:3020"
+// export const API_URL = "http://localhost:3020/api"; // aca va la direccion del backend donde esta la api
+export const IMAGES_API_URL = "http://localhost:3020";
+// export const API_URL = "https://materialesback.onrender.com/api";
+export const API_URL = import.meta.env.VITE_API_URL;
